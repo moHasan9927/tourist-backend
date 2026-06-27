e7b6b442128cb60cdadf6da60047168812352621
Mhasan9927 <s20502002649@nu.ac.bd>
2026-06-27 23:09:11 +0600

    Fixed Backend

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
+require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
 const { MongoClient, ObjectId, ServerApiVersion } = require("mongodb");
-
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
-
-const uri = "mongodb://localhost:27017";
-
+// mongodb://localhost:27017
+//
+const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
